394f79db4b3d94752c67948f77edaecc7731b50b
Matthew Taylor <matthewtaylor98.mt@gmail.com>
2018-06-01 16:42:55 +0100

    Added Tests for CashPayment - Needs fixing currently Wont compile
    LNK2019

---
+++ UnitTesting/CashPaymentTest.cpp
@@ -0,0 +1,28 @@
+﻿#pragma once
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../Vending Machine/CashPayment.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace CashPaymentTestSystem
+{
+	TEST_CLASS(CashPaymentTest)
+	{
+	public:
+
+	/*	TEST_METHOD(CashPaymentTest_ExpectedIntValueReturned)
+		{
+			auto cashPayment = CashPayment(100);
+			const int expectedValue = 100;
+			Assert::AreEqual(expectedValue, cashPayment.returnPaymentValue());
+		};*/
+
+	/*	TEST_METHOD(CashPaymentTest_ExpectedBoolValueReturned)
+		{
+			auto cashPayment = CashPayment(1);
+			auto result = cashPayment.ChangeNeeded();
+			const bool expectedValue = true;
+			Assert::AreEqual(expectedValue, result);
+		};*/
+	};
+}
